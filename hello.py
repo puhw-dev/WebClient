258f2c7bb2ba95fe9bc481ea29eaa8c371120b8f
Pawel Piecyk <pawel@piecyk.net>
2014-04-01 23:43:09 +0200

    Json sinus

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, jsonify, Response
+import json
+import math
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,10 +13,12 @@ def templating():
     app.logger.debug("Omg, I'm loggin somethin... ;-)")
     return render_template('hello.html', name="Wiktorek")
 
-@app.route("/json")
-def json():
-    tab = ['a', 'b']
-    return tab 
+@app.route("/json_sin")
+def json_sin():
+    values = [["y", "y"]]
+    for x in range(0, 100):
+        values.append([x/10., math.sin(x/10.)])
+    return Response(json.dumps(values), mimetype='application/json')
 
 if __name__ == "__main__":
     app.run(use_debugger=True, use_reloader=True, debug=True)
