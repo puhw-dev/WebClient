3a3064936722e1bb9ba1787b1525d2e7c0610060
Pawel Piecyk <pawel@piecyk.net>
2014-04-02 22:12:23 +0200

    Dummy sinus plot

--- hello.py
+++ hello.py
@@ -2,22 +2,23 @@ from flask import Flask
 from flask import render_template, jsonify, Response
 import json
 import math
+from random import random
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, this is first view, or maybe the second? ;-)"
+    return render_template('index.html')
 
-@app.route("/templating")
-def templating():
-    app.logger.debug("Omg, I'm loggin somethin... ;-)")
-    return render_template('hello.html', name="Wiktorek")
+# @app.route("/templating")
+# def templating():
+#     app.logger.debug("Omg, I'm loggin somethin... ;-)")
+#     return render_template('hello.html', name="Wiktorek")
 
 @app.route("/json_sin")
 def json_sin():
-    values = [["y", "y"]]
+    values = [["x", "y"]]
     for x in range(0, 100):
-        values.append([x/10., math.sin(x/10.)])
+        values.append([x/10., math.sin(x/10.) + random()])
     return Response(json.dumps(values), mimetype='application/json')
 
 if __name__ == "__main__":
