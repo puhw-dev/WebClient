6c68a890da7adc807a102e2966123547840f5117
Pawel Piecyk <nickel32@gmail.com>
2014-05-29 14:03:20 +0200

    #5 Methods for getting hosts, sensors and metrics extracted

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ import os
 
 CATALOG_HOST = os.getenv('CATALOG_HOST')
 REQUEST_TIMEOUT = 10  # request timeout in seconds
+SEARCH_DATA_REFRESH_INTERVAL = 120
--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def hosts_list():
     monitor_ip = request.args["ip"]
 
     try:
-        hosts_list_data = json.loads(monitor_get_request(monitor_ip, "/hosts"))
+        hosts_list_data = json.loads(get_hosts(monitor_ip))
     except requests.Timeout:
         return Response("Timeout", 408)
 
@@ -58,13 +58,14 @@ def hosts_list():
     return Response(json.dumps(list_of_hosts), mimetype='application/json')
 
 
+
 @app.route("/monitor/hosts/<hostname>/sensors/")
 def sensors_list(hostname):
     """Returns sensors list for specified host"""
     monitor_ip = request.args["ip"]
 
     try:
-        sensors_list_data = monitor_get_request(monitor_ip, "/hosts/" + hostname + "/sensors")
+        sensors_list_data = get_sensors(monitor_ip, hostname)
     except requests.Timeout:
         return Response("Timeout", 408)
 
@@ -77,7 +78,7 @@ def metrics_list(hostname, sensorname):
     monitor_ip = request.args["ip"]
 
     try:
-        metrics_list_data = monitor_get_request(monitor_ip, "/hosts/" + hostname + "/sensors/" + sensorname + "/metrics")
+        metrics_list_data = get_metrics(monitor_ip, hostname, sensorname)
     except requests.Timeout:
         return Response("Timeout", 408)
 
@@ -91,8 +92,20 @@ def monitor_get_request(monitor_ip, url):
     return get_request.text
 
 
+def get_hosts(monitor_ip):
+    return monitor_get_request(monitor_ip, "/hosts")
+
+
+def get_sensors(monitor_ip, hostname):
+    return monitor_get_request(monitor_ip, "/hosts/" + hostname + "/sensors")
+
+
+def get_metrics(monitor_ip, hostname, sensorname):
+    return monitor_get_request(monitor_ip, "/hosts/" + hostname + "/sensors/" + sensorname + "/metrics")
+
 @app.route("/search")
 def search():
+    """Method which filters search results from file"""
     search_data_file = open('search_data.json', 'r')
     search_data = json.load(search_data_file)
 
@@ -113,6 +126,13 @@ def json_sin():
 
 
 def generate_search_data():
+    """Generates data for search functionality.
+    It asks each monitors for hosts, host for sensors and sensors for metrics.
+    Saves data in file (JSON format).
+    It's really simple solution and I'm aware of the fact that it isn't the best regarding performance and scalability
+    but as long as we've got only a few monitors, sensors and metrics it should be okay, otherwise it would be nice
+    to have sth faster, probably based on db."""
+
     log.info("Generating search data")
 
     catalog_address = app.config.get("CATALOG_HOST")
@@ -126,29 +146,26 @@ def generate_search_data():
     # for each monitor get list of hosts
     for monitor in monitors:
         try:
-            hosts_data = monitor_get_request(monitor["ip"], "/hosts")
-            hosts_data_list = json.loads(hosts_data)
+            hosts_data = json.loads(get_hosts(monitor["ip"]))
 
             # for each host get list of sensors
-            for host in hosts_data_list["hosts"]:
+            for host in hosts_data["hosts"]:
                 search_data.append({"type": "host", "monitor": monitor["name"], "name": host["hostname"]})
 
-                sensors_data = monitor_get_request(monitor["ip"], "/hosts/" + host["hostname"] + "/sensors")
-                sensors_data_list = json.loads(sensors_data)
+                sensors_data = json.loads(get_sensors(monitor["ip"], host["hostname"]))
 
                 # for each sensor get list of metrics
-                for sensor in sensors_data_list["sensors"]:
+                for sensor in sensors_data["sensors"]:
                     search_data.append({"type": "sensor", "monitor": monitor["name"], "host": host["hostname"], "name": sensor["sensorname"]})
 
-                    metrics_data = monitor_get_request(monitor["ip"], "/hosts/" + host["hostname"] + "/sensors/" + sensor["sensorname"] + "/metrics")
-                    metrics_data_list = json.loads(metrics_data)
+                    metrics_data = json.loads(get_metrics(monitor["ip"], host["hostname"], sensor["sensorname"]))
 
-                    for metric in metrics_data_list["metrics"]:
-                        search_data.append({"type": "metric", "monitor": monitor["name"], "host": host["hostname"], "sensorname": sensor["sensorname"], "name": metric["name"]})
+                    for metric in metrics_data["metrics"]:
+                        search_data.append({"type": "metric", "monitor": monitor["name"], "host": host["hostname"], "sensor": sensor["sensorname"], "name": metric["name"]})
 
         except requests.Timeout:
             pass  # Just ignore timeouts, if any of endpoints is down we can't do anything about that.
-            # It's also quite good protection from invalid data about monitors (during development).
+                  # It's also quite good protection from invalid data about monitors (during development).
 
     search_data_file = open('search_data.json', 'w')
     search_data_file.write(json.dumps(search_data))
@@ -159,7 +176,7 @@ def generate_search_data():
 def generate_search_data_background_task():
     while True:
         generate_search_data()
-        time.sleep(60)
+        time.sleep(app.config.get('SEARCH_DATA_REFRESH_INTERVAL', 120))
 
 
 if __name__ == "__main__":
@@ -173,6 +190,6 @@ if __name__ == "__main__":
     port = int(os.getenv('PORT', 80))
     env = os.getenv('PUHW_ENV', 'DEV')
     if env == 'DEV':
-        app.run(use_debugger=True, use_reloader=False, debug=True, port=port)
+        app.run(use_debugger=True, use_reloader=True, debug=True, port=port)
     else:
         app.run(host='0.0.0.0', use_debugger=False, use_reloader=False, debug=False, port=port)
